2296d918fc796578d586fa6a708298c77fc0a3e3
shubhamedtech <shubham@edtechinnovate.com>
2025-03-03 14:55:49 +0530

    otp work

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\OTPController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/get-otp/{mobile_no}',[OTPController::class,'getOtp'])->name('get-otp');
+Route::get('/verify-otp/{otp}/{mobile_no}',[OTPController::class,'verifyOtp']);
