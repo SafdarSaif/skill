e2661256af07e1cbb8a59c20f6f9a225b8387850
karuna <karunapatel442@gmail.com>
2025-03-08 14:35:32 +0530

    commit

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\StudentPaymentController;
 use App\Http\Controllers\StudentsController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\EasebuzzPaymentController;
+use App\Http\Controllers\SubjectController;
+// use App\Http\Controllers\EasebuzzController;
 
 use App\Models\Students;
 use Illuminate\Http\Request;
@@ -21,14 +23,19 @@ Route::get('/students-details/{mobile_no}',[StudentsController::class,'getStuden
 Route::post('/students-registration',[StudentsController::class,'registerStudent']);
 Route::get('/students-all-details/{mobile_no}',[StudentsController::class,'StudentAllDetaills']);
 Route::get('/students-payment/{mobile_no}',[StudentPaymentController::class,'StudentPayment']);
-Route::get('/pay-student-course-fee/{student_id}/{course_id}',[CourseController::class,'payStuCourseFee']);
+Route::post('/pay-student-course-fee',[CourseController::class,'payStuCourseFee']);
 Route::get('/all-categories',[CategoryController::class,'categories']);
 Route::get('/all-courses/{column}/{value}',[CourseController::class,'coursesFunc']);
 Route::get('/all-courses',[CourseController::class,'coursesFunc']);
-Route::post('/api/category-courses',[CategoryController::class,'getCategoryCourses']);
-
+Route::post('/category-courses',[CategoryController::class,'getCategoryCourses']);
+Route::get('/course-wise-subjects', [SubjectController::class,'getCourseSubjects']);
 
 Route::post('/pay', [EasebuzzPaymentController::class, 'initiatePayment'])->name('easebuzz.pay');
 Route::post('/payment-success', [EasebuzzPaymentController::class, 'paymentSuccess'])->name('easebuzz.success');
 Route::post('/payment-failure', [EasebuzzPaymentController::class, 'paymentFailure'])->name('easebuzz.failure');
 
+Route::get('/easebuzz/initiate', [EasebuzzPaymentController::class, 'initiate_payment_show']);
+Route::post('/easebuzz/initiate', [EasebuzzPaymentController::class, 'initiate_payment_ebz']);
+Route::post('/easebuzz/response', [EasebuzzPaymentController::class, 'ebz_response']);
+Route::post('/easebuzz/success', [EasebuzzPaymentController::class, 'payment_success']);
+Route::post('/easebuzz/failure', [EasebuzzPaymentController::class, 'payment_failure']);
