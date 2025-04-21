b77d072c81830b2baf1f428a54d74f16908304ef
safdar <safdar@edtechinnovate.com>
2025-04-21 14:53:10 +0530

    migration changes

--- database/migrations/2025_04_21_090727_update_course_id_foreign_in_student_progress.php
+++ database/migrations/2025_04_21_090727_update_course_id_foreign_in_student_progress.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,12 +13,17 @@ return new class extends Migration
         Schema::table('student_progress', function (Blueprint $table) {
             $table->dropForeign(['course_id']);
 
+            $table->dropIndex(['course_id']); 
+
+            $table->index('course_id');
+
             $table->foreign('course_id')
                 ->references('id')->on('courses')
                 ->onDelete('cascade')
                 ->onUpdate('restrict');
         });
     }
+
     /**
      * Reverse the migrations.
      */
@@ -27,6 +31,10 @@ return new class extends Migration
     {
         Schema::table('student_progress', function (Blueprint $table) {
             $table->dropForeign(['course_id']);
+
+            $table->dropIndex(['course_id']);
+
+            $table->index('course_id');
 
             $table->foreign('course_id')
                 ->references('id')->on('student_courses')
